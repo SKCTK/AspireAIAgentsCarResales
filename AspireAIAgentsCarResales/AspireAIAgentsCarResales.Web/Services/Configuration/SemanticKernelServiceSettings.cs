74cd84549892a8889da1774f9508cd1ef850ef43
Hon Lee <hon.lee@capgemini.com>
2025-04-30 23:14:20 +0100

    update

---
+++ AspireAIAgentsCarResales/AspireAIAgentsCarResales.Web/Services/Configuration/SemanticKernelServiceSettings.cs
@@ -0,0 +1,10 @@
+﻿using Aspire.Azure.AI.OpenAI;
+
+namespace AspireAIAgentsCarResales.Web.Services.Configuration
+{
+    public record SemanticKernelServiceSettings
+    {
+        public required AzureOpenAISettings AzureOpenAISettings { get; init; }
+        public required CosmosDBSettings CosmosDBVectorStoreSettings { get; init; }
+    }
+}
---
+++ AspireAIAgentsCarResales/AspireAIAgentsCarResales.Web/Services/Models/AgentType.cs
@@ -0,0 +1,12 @@
+﻿namespace AspireAIAgentsCarResales.Web.Services.Models
+{
+    enum AgentType
+    {
+        RegionalDemand = 0,
+        EconomicImpact = 1,
+        CompetitiveMarket = 2,
+        FinalReview = 3,
+        SelectionStrategy = 4,
+        TerminationStrategy = 5
+    }
+}
